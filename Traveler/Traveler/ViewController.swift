5c12da92e1846cfb1a0a240ddb1837585f17f69d
sherifaaljassem <70379109+sherifaaljassem@users.noreply.github.com>
2020-10-14 22:10:35 +0300

    updated

---
+++ Traveler/Traveler/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Traveler
+//
+//  Created by Sherifa Aljassem on 10/14/20.
+//  Copyright © 2020 Sherifa Aljassem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UIImageView!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signAsGuest: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Traveler/Traveler/vc1.swift
@@ -0,0 +1,10 @@
+//
+//  vc1.swift
+//  Traveler
+//
+//  Created by Sherifa Aljassem on 10/14/20.
+//  Copyright © 2020 Sherifa Aljassem. All rights reserved.
+//
+
+import Foundation
+struct 
