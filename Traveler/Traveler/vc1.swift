e45e326d464e62b80ca15efc057dca3abe96dcf7
sherifaaljassem <70379109+sherifaaljassem@users.noreply.github.com>
2020-10-21 13:17:35 +0300

    updated

--- Traveler/Traveler/vc1.swift
+++ Traveler/Traveler/vc1.swift
@@ -20,8 +20,15 @@ struct country{
 var turkey = [country(area: "bursa", restuarant: ["zennap", "uzanEtMangal", "egeBalik"], mall: ["kentMeydani", "zafer", "marka"], parks: ["uludag", "golyazi"]),
               country(area: "istanbul", restuarant: ["bigChef", "balik", "kababjiMahmoud"], mall: ["mallOfIstanbul", "Akbati", "Istinye"], parks: ["yildiz", "emirgan", "gulhane"]),country(area: "ankara", restuarant: ["ekmekArasi","trilye"], mall: ["kentPark", "gordian"], parks: ["eymir", "dickmenVadisi"])]
 
-var germany = [country(area: "bursa", restuarant: ["zennap", "uzanEtMangal", "egeBalik"], mall: ["kentMeydani", "zafer", "marka"], parks: ["uludag", "golyazi"]),
-country(area: "istanbul", restuarant: ["bigChef", "balik", "kababjiMahmoud"], mall: ["mallOfIstanbul", "Akbati", "Istinye"], parks: ["yildiz", "emirgan", "gulhane"]),country(area: "ankara", restuarant: ["ekmekArasi","trilye"], mall: ["kentPark", "gordian"], parks: ["eymir", "dickmenVadisi"])]
 
- var britain = [country(area: "bursa", restuarant: ["zennap", "uzanEtMangal", "egeBalik"], mall: ["kentMeydani", "zafer", "marka"], parks: ["uludag", "golyazi"]),
- country(area: "istanbul", restuarant: ["bigChef", "balik", "kababjiMahmoud"], mall: ["mallOfIstanbul", "Akbati", "Istinye"], parks: ["yildiz", "emirgan", "gulhane"]),country(area: "ankara", restuarant: ["ekmekArasi","trilye"], mall: ["kentPark", "gordian"], parks: ["eymir", "dickmenVadisi"])]
+var germany = [country(area: "berlin", restuarant: ["skyKitchen", "restaurantFacil", "aMano"], mall: ["mallOfBerlin", "potsdamerPlatz", "bikiniBerlin"], parks: ["gartenDelWelt", "charlottenburgPalace"]),
+               
+country(area: "hamburg", restuarant: ["haerlin", "sagners", "philipps"], mall: ["phoenixCenter", "billstedt", "levantehaus"], parks: ["plantenUnBlomen", "altonaerVolkspark", "jenisch"])
+    
+    ,country(area: "munich", restuarant: ["luSalentinuBistro","taverna"], mall: ["hofstatt", "stachusPassagen"], parks: ["englishGarten", "hofgarten"])]
+
+ var britain = [country(area: "london", restuarant: ["chateau", "theLedbury", "sketch"], mall: ["westFieldLondon", "cardinalPlace", "oneNewChange"], parks: ["victoria", "battersea"]),
+                
+ country(area: "scotland", restuarant: ["theKitchin", "condita", "peatInn"], mall: ["waverleyMall", "fortKinnaird", "theThistlesShoppingMall"], parks: ["queenElizabethForestPark", "cairngorms"])
+    
+    ,country(area: "wales", restuarant: ["theWalnutTreeInn","theWhiteBrook"], mall: ["cwmbranCentre", "tweedmill"], parks: ["breconBeacons", "weprePark"])]
